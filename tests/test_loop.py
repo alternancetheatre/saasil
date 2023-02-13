ef9dde16111d9f361c982eb2e303e19b164c4459
pi3-02 <webmaster@alternancetheatre.org>
2023-02-13 19:04:55 +0100

    add delay after init

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -6,8 +6,10 @@ test.restart()
 
 test.move_servo_position(0, 0)
 
+time.sleep(1)
+
 while True:
     test.move_servo_position(0,130)
-    time.sleep(5)
+    time.sleep(3)
     test.move_servo_position(0,0)
-    time.sleep(5)
+    time.sleep(3)
